9fd54d0e32266399210bcc8eb053db591a82e26e
regis066 <mugisharegis72@gmail.com>
2024-10-22 11:47:36 +0200

    Register the models to the admin interface

--- service_hours/admin.py
+++ service_hours/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from service_hours.models import ServiceHour
 
 # Register your models here.
+admin.register(ServiceHour)
--- volunteers/admin.py
+++ volunteers/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from volunteers.models import Volunteer
 
 # Register your models here.
+admin.register(Volunteer)
