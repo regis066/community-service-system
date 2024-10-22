7558ac5c71e6ec4c183fd250d7a68f8642ed7ff1
regis066 <mugisharegis72@gmail.com>
2024-10-22 11:35:10 +0200

    Add ServiceHour model for service_hours app

--- service_hours/models.py
+++ service_hours/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from projects.models import Project
+from volunteers.models import Volunteer
+
 
 # Create your models here.
+class ServiceHour(models.Model):
+    volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    hours = models.DecimalField(max_digits=5, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.volunteer.name} - {self.hours} hours"
